6ae47640445b7d25ddde0a5cba0443a5751767fa
Slaybesh <33007237+Slaybesh@users.noreply.github.com>
2018-10-13 14:15:52 +0200

    Testcommit

--- tasker/disengage.js
+++ tasker/disengage.js
@@ -136,7 +136,7 @@ disengage()
 function engage() {
     setGlobal('Disengaged', false);
     enableProfile('Engage', false);
-    shell('settings put secure accessibility_display_daltonizer_enabled 0', true);
+    shell('settings put secure accessibility_display_daltonizer_enabled 0hyg', true);
     performTask('regular_checks')
     // setWifi(true);
     // mobileData(true);
