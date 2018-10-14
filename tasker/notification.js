3880eac627d2771e3d5200a791494793f41fab00
Huy Hoang <huyhoang0496@gmail.com>
2018-10-14 17:54:42 +0200

    tasker

--- tasker/notification.js
+++ tasker/notification.js
@@ -12,13 +12,35 @@ var icon = par1.icon;
 var priority = par1.priority;
 flash(title)
 
+
+var snooze_time;
 function remove_notifications() {
-    let glob_snooze_time = parseInt(global('Snooze_time'));
-    let glob_times = parseInt(global('TIMES'));
-    let glob_disengaged_until = global('Disengaged_until');
-    if (global('Disengaged') == 'true' || glob_snooze_time > glob_times) {
-        if ( glob_disengaged_until > glob_time ) {
-            var snooze_time 
+    let Snooze_time = parseInt(global('Snooze_time'));
+    let TIMES = parseInt(global('TIMES'));
+    let Disengaged_until = parseInt(global('Disengaged_until'));
+    let Disengaged = (global('Disengaged') === 'true');
+
+    if (Disengaged || Snooze_time > TIMES) {
+        if (Disengaged_until > TIMES) {
+            snooze_time = (Disengaged_until + 60 - TIMES) * 1000;
+        } else if (Disengaged) {
+            snooze_time = 1800 * 1000;
+        } else {
+            snooze_time = (Snooze_time + 5 - TIMES) * 1000;
         }
+
+        let packages = global('All_notification_packages')
+        for (const i in packages) {
+            flash(packages[i]);
+        }
+            
     }
+}
+
+function flash(input) {console.log(input)}
+
+var arr = [0,1,2,3]
+flash(arr)
+for (const i in arr) {
+    flash(arr[i]);
 }
--- tasker/testing.js
+++ tasker/testing.js
@@ -1,5 +1,17 @@
-var global_var = 'glob';
-function changer() {
-    global_var = 'changed';
+let arr = [123, 321, 'asd', 'dsa']
+
+// for (const i in arr) {
+//     console.log(i, arr[i]);
+//   }
+
+var obj = {
+    a: 1,
+    b: 2,
+    c: 3
+};
+
+
+for (prop in obj) {
+    var prop = obj;
+    console.log(prop)
 }
-changer()
