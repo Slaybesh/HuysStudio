c5d7a33a2aa85816c450cbc8f2ef133b4ed98f47
Huy <huy_hoang@neuralaim.com>
2018-10-16 12:43:36 +0200

    t

--- tasker/testing.js
+++ tasker/testing.js
@@ -7,9 +7,11 @@
 function sleep(ms) {return new Promise(resolve => setTimeout(resolve, ms))}
 
 async function loop() {
+    let start_time = parseInt(global('TIMES'));
     while (true) {
-        flash('');
-        sleep(3000);
+        let new_time = parseInt(global('TIMES'));
+        flash(new_time - start_time);
+        await sleep(3000);
     }
-
 }
+loop();
