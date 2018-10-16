184c86c0f14aadd1dbf2685b8ad19f2de675715d
Huy <huy_hoang@neuralaim.com>
2018-10-16 14:22:20 +0200

    t

--- tasker/testing/event_loop.js
+++ tasker/testing/event_loop.js
@@ -1,16 +1,19 @@
 function sleep(ms) {return new Promise(resolve => setTimeout(resolve, ms))}
-function flash(msg) {console.log(msg)}
+// function flash(msg) {console.log(msg)}
 
 function task1(){
     flash('task1')
+    exit()
 }
 function task2(){
     flash('task2')
+    exit()
 }
 async function long_task(){
     flash('beginning long task')
     await sleep(2000)
     flash('finishing long task')
+    exit()
 }
 
 async function loop(){
@@ -18,10 +21,11 @@ async function loop(){
         flash('loop ' + i)
         await sleep(3000)
     }
+    exit()
 }
 
 function event_loop(fn){
     eval(fn + '()')
 }
-flash(par1)
-event_loop(par1)
+flash(fn_name)
+event_loop(fn_name)
