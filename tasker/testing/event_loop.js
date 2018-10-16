6e0fc54b77055e439d0cafb14e40bdf29a05ed16
Huy Hoang <huyhoang0496@gmail.com>
2018-10-16 14:33:52 +0200

    t

--- tasker/testing/event_loop.js
+++ tasker/testing/event_loop.js
@@ -2,30 +2,30 @@ function sleep(ms) {return new Promise(resolve => setTimeout(resolve, ms))}
 // function flash(msg) {console.log(msg)}
 
 function task1(){
-    flash('task1')
-    exit()
+    flash('task1');
+    exit();
 }
 function task2(){
-    flash('task2')
-    exit()
+    flash('task2');
+    exit();
 }
 async function long_task(){
-    flash('beginning long task')
-    await sleep(2000)
-    flash('finishing long task')
-    exit()
+    flash('beginning long task');
+    await sleep(2000);
+    flash('finishing long task');
+    exit();
 }
 
 async function loop(){
     for (i=0; i<4; i++){
-        flash('loop ' + i)
-        await sleep(3000)
+        flash('loop ' + i);
+        await sleep(3000);
     }
     exit()
 }
 
 function event_loop(fn){
-    eval(fn + '()')
+    eval(fn + '()');
 }
-flash(fn_name)
+// flash(fn_name)
 event_loop(fn_name)
