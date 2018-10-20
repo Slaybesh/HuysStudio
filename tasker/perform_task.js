1f03d264ea77ccc7b645438285dfc82f6a495f9c
Huy Hoang <huyhoang0496@gmail.com>
2018-10-20 18:09:19 +0200

    perf task

--- tasker/perform_task.js
+++ tasker/perform_task.js
@@ -1,2 +1,12 @@
-performTask('Some Task');
-flash(test_var)
+function sleep(ms) {return new Promise(resolve => setTimeout(resolve, ms))}
+
+async function tasker(task_name) {
+    flash('Starting: ' + task_name);
+    performTask(task_name);
+    while (global('TRUN').includes(task_name)) {
+        await sleep(100);
+    }
+    flash('Finished: ' + task_name);
+}
+
+tasker(some_task)
