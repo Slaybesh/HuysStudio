3669668617881bb43714e2371de5dcf9b3cf3040
Huy Hoang <huyhoang0496@gmail.com>
2018-10-23 18:33:10 +0200

    +

--- tasker/notifications/create_notifications.js
+++ tasker/notifications/create_notifications.js
@@ -16,21 +16,21 @@ create()
 
 async function create() {
     // var text;
-    launch_task('create_notification', 5, `Timer|0|mw_image_timer|3`);
+    await launch_task('create_notification', 5, `Timer|0|mw_image_timer|3`);
     await sleep(1000)
-    launch_task('create_notification', 5, `Timer|1|mw_image_timer|3`);
+    await launch_task('create_notification', 5, `Timer|1|mw_image_timer|3`);
     await sleep(1000)
-    launch_task('create_notification', 5, `Timer|2|mw_image_timer|3`);
+    await launch_task('create_notification', 5, `Timer|2|mw_image_timer|3`);
     exit();
 }
 
 async function launch_task(task_name, priority=5, par1='', par2='') {
-    // if (debugging) {flash('Starting: ' + task_name)}
+    if (debugging) {flash('Starting: ' + task_name)}
     
     performTask(task_name, priority, par1, par2);
     while (global('TRUN').includes(task_name)) {await sleep(100)}
 
-    // if (debugging) {flash('Finished: ' + task_name)}
+    if (debugging) {flash('Finished: ' + task_name)}
 }
 
 // function performTask(task_name, priority, par1, par2) {
