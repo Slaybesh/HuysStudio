e167d3653a0ead89f1a79e99be83ecd61f8b58af
Huy Hoang <huyhoang0496@gmail.com>
2018-10-23 16:20:13 +0200

    reg checks

--- tasker/main/regular_checks.js
+++ tasker/main/regular_checks.js
@@ -3,13 +3,18 @@ function sleep(ms) {return new Promise(resolve => setTimeout(resolve, ms))}
 function regular_checks() {
     shell('settings put secure enabled_accessibility_services' + global('Accessibility_services'));
     roundr();
-    performTask('remove_persistent_notifications');
-    performTask('zooper_disengaged');
-    performTask('zooper_reload_location');
+    performTask('Remove Persistent');
+    performTask('Zooper Disengaged');
+    performTask('Zooper Reload Location');
+    // performTask('remove_persistent_notifications');
+    // performTask('zooper_disengaged');
+    // performTask('zooper_reload_location');
 }
 
 async function roundr() {
     let id_roundr = shell('echo proc/$(pidof mohammad.adib.roundr) | cut -f 2 -d "/"');
+    flash(id_roundr);
+    flash(!id_roundr.match(/[0-9]+/));
     if (!id_roundr.match(/[0-9]+/)) {
         loadApp('Roundr', '', true);
         performTask('Remove Persistent');
