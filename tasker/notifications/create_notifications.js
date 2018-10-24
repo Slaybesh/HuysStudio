274013abceba2d3c40e13255bc832ca0a6e250df
Huy <huyhoang0496@gmail.com>
2018-10-25 00:57:41 +0200

    create not

--- tasker/notifications/create_notifications.js
+++ tasker/notifications/create_notifications.js
@@ -2,9 +2,10 @@ function sleep(ms) {return new Promise(resolve => setTimeout(resolve, ms))}
 
 async function timer () {
     let i = 0;
+    let new_priority = parseInt(priority) + 1;
     while (i < 10) {
         
-        performTask('create_notification', 5, `Timer|${i}|mw_image_timer|3`);
+        performTask('create_notification', new_priority, `Timer|${i}|mw_image_timer|2`);
         i++;
         // flash(i);
         await sleep(1000);
@@ -12,25 +13,27 @@ async function timer () {
     exit();
 }
 
-create()
+timer()
 
 async function create() {
     // var text;
-    await launch_task('create_notification', 5, `Timer|0|mw_image_timer|3`);
-    await sleep(1000)
-    await launch_task('create_notification', 5, `Timer|1|mw_image_timer|3`);
-    await sleep(1000)
-    await launch_task('create_notification', 5, `Timer|2|mw_image_timer|3`);
+    let new_priority = parseInt(priority) + 1;
+    flash(new_priority);
+    performTask('create_notification', new_priority, `Timer0|0|mw_image_timer|3`);
+    await sleep(1000);
+    performTask('create_notification', new_priority, `Timer1|1|mw_image_timer|3`);
+    await sleep(1000);
+    performTask('create_notification', new_priority, `Timer2|2|mw_image_timer|3`);
     exit();
 }
 
 async function launch_task(task_name, priority=5, par1='', par2='') {
-    if (debugging) {flash('Starting: ' + task_name)}
+    //if (debugging) {flash('Starting: ' + task_name)}
     
     performTask(task_name, priority, par1, par2);
     while (global('TRUN').includes(task_name)) {await sleep(100)}
 
-    if (debugging) {flash('Finished: ' + task_name)}
+    //if (debugging) {flash('Finished: ' + task_name)}
 }
 
 // function performTask(task_name, priority, par1, par2) {
