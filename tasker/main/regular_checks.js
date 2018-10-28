b38eb352c1d2827a515d23ef7f1023afebdccc37
Huy Hoang <huyhoang0496@gmail.com>
2018-10-28 21:57:13 +0100

    +

--- tasker/main/regular_checks.js
+++ tasker/main/regular_checks.js
@@ -1,31 +1,42 @@
 function sleep(ms) {return new Promise(resolve => setTimeout(resolve, ms))}
 
-// var date = new Date(); 
-// let time = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds() + ":" + date.getMilliseconds();
-// function logger()
 
+function create_logger(path) {
+    function logger(msg) {
+        var date = new Date(); 
+        let time = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds() + ":" + date.getMilliseconds();
+        writeFile(path, `${time} ${msg}\n`, true);
+    }
+}
+
+logger = create_logger('Tasker/log/regular_checks.txt');
 
 async function regular_checks() {
-    writeFile('Tasker/log/regular_checks.txt', 'start regular_checks\n', true)
+    // writeFile('Tasker/log/regular_checks.txt', 'start regular_checks\n', true)
+    logger('start regular_checks')
     shell('settings put secure enabled_accessibility_services' + global('Accessibility_services'));
     await roundr();
     await remove_persistent();
     performTask('Zooper Disengaged');
     performTask('Zooper Reload Location');
-    writeFile('Tasker/log/regular_checks.txt', 'end regular_checks\n', true)
+    logger('end regular_checks')
+    // writeFile('Tasker/log/regular_checks.txt', 'end regular_checks\n', true)
 }
 
 async function remove_persistent() {
-    writeFile('Tasker/log/regular_checks.txt', 'start remove_persistent\n', true)
+    logger('start remove_persistent')
+    // writeFile('Tasker/log/regular_checks.txt', 'start remove_persistent\n', true)
     let persistent_apps = JSON.parse(global('Apps_persistent'));
     // writeFile('Tasker/log/regular_checks.txt', persistent_apps + '\n', true)
     for (i in persistent_apps) {
         performTask('Notification.snooze', parseInt(priority) + 1, persistent_apps[i], 10000000000000);
     }
-    writeFile('Tasker/log/regular_checks.txt', 'end remove_persistent\n', true)
+    logger('end remove_persistent')
+    // writeFile('Tasker/log/regular_checks.txt', 'end remove_persistent\n', true)
 }
 async function roundr() {
-    writeFile('Tasker/log/regular_checks.txt', 'start roundr\n', true)
+    logger('start roundr')
+    // writeFile('Tasker/log/regular_checks.txt', 'start roundr\n', true)
     let id_roundr = shell("echo proc/$(pidof mohammad.adib.roundr) | cut -f 2 -d '/'", true);
     // flash(id_roundr);
     if (!id_roundr.match(/[0-9]+/)) {
@@ -33,10 +44,12 @@ async function roundr() {
         await sleep(100);
         button('back');
     }
-    writeFile('Tasker/log/regular_checks.txt', 'end roundr\n', true)
+    logger('end roundr')
+    // writeFile('Tasker/log/regular_checks.txt', 'end roundr\n', true)
 }
 
 regular_checks().then(() => {
-    writeFile('Tasker/log/regular_checks.txt', '\n', true);
+    logger('\n')
+    // writeFile('Tasker/log/regular_checks.txt', '\n', true);
     exit();
 });
