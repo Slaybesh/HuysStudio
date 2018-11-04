d0b13c2b1ad1c3be0117075aa340e38b044f25c4
Huy Hoang <huyhoang0496@gmail.com>
2018-11-04 14:29:09 +0100

    +

--- tasker/Scenes/app_button.js
+++ tasker/Scenes/app_button.js
@@ -1,15 +1,16 @@
 function sleep(ms) {return new Promise(resolve => setTimeout(resolve, ms))}
 
 async function onPress() {
-    await sleep(250)
+    // await sleep(200)
     if (math_input == math_result) {
         hideScene('app')
     } else {
+
         elemVisibility('app', 'Math Input', false, 200)
         elemText('app', 'Math Input', 'repl', '');
         elemBorder('app', 'Math Input', 1, 'ff0000')
         elemVisibility('app', 'Math Input', true, 200)
-        await sleep(500)
+        await sleep(1000)
         elemVisibility('app', 'Math Input', false, 200)
         elemBorder('app', 'Math Input', 1, '000000')
         elemVisibility('app', 'Math Input', true, 0)
