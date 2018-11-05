22ce7bce43c5edc95872ab19f7dc73cc9561acaf
Huy Hoang <huyhoang0496@gmail.com>
2018-11-05 16:27:22 +0100

    +

--- tasker/testing/test_speed.js
+++ tasker/testing/test_speed.js
@@ -1,7 +1,7 @@
 function sleep(ms) {return new Promise(resolve => setTimeout(resolve, ms))}
 
 async function wait(x) {
-    for (i=0; i=x; i++) {
+    for (i=0; i<x; i++) {
         await sleep(1000)
     }
     exit();
