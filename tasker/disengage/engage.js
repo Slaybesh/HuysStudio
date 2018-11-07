d8b3c1eebfa2482a4eef28ead64a5832a2d544c4
Huy Hoang <huyhoang0496@gmail.com>
2018-11-07 17:41:29 +0100

    remove tasker

--- tasker/disengage/engage.js
+++
@@ -1,9 +0,0 @@
-function engage() {
-    setGlobal('Disengaged', 0);
-    enableProfile('Engage', false);
-    shell('settings put secure accessibility_display_daltonizer_enabled 0', true);
-    performTask('regular_checks');
-    // setWifi(true);
-    // mobileData(true);
-}
-engage()
--- tasker/tasks/AutoInput_UI_Query.js
+++
@@ -1,9 +0,0 @@
-var aipackage;
-var aiapp;
-
-let json = {
-    package: aipackage,
-    app: aiapp
-}
-
-setGlobal('Return_AutoInput_UI_Query', JSON.stringify(json, null, 2));
